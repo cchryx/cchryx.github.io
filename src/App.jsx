0efde26e6ec22575ccb6dccd67069a0bec53d341
Chris Chen <chris.chen3128@gmail.com>
2024-07-15 00:31:18 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./app.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Landing from "./pages/Landing/Landing";
 import Projects from "./pages/Projects/Projects";
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Landing />}></Route>
-            <Route path="/projects" exact element={<Projects />}></Route>
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Landing />}></Route>
+                <Route path="/projects" exact element={<Projects />}></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
